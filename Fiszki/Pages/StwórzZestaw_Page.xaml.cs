29cf2a6a050cd564c366c79e81b12be110354dfc
Jakub Wierzbicki <j.wierzbicki2001@gmail.com>
2022-07-01 01:32:20 +0200

    Dodano Strony i kontrolki w Projekcie Głównym

---
+++ Fiszki/Pages/StwórzZestaw_Page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fiszki
+{
+    /// <summary>
+    /// Logika interakcji dla klasy StwórzZestaw_Page.xaml
+    /// </summary>
+    public partial class StwórzZestaw_Page : Page
+    {
+        public StwórzZestaw_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
